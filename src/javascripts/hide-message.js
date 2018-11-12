0a2a1e30bcbd11e562f47154eb1c35f215f5c1b9
Victor Neves <info@vitorneves.com>
2018-11-12 11:07:04 +0000

    Deleted unused files

--- src/javascripts/hide-message.js
+++
@@ -1,9 +0,0 @@
-let hideMessage = (results) => {
-
-  if(results === 0) {
-    document.getElementById('js-no-results').style.display = "flex";;
-  } else {
-    document.getElementById('js-no-results').style.display = "none";;
-  }
-
-}
